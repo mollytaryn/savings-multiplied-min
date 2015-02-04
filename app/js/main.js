374308f12bfeed1bfb6c404cea9b7df37b905712
mollytaryn <mollytaryn@gmail.com>
2015-02-04 14:46:32 -0600

    added our libraries, made our first container display data

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,26 @@
 (function(){
-
   'use strict';
-  $.get('https://savingsmultipliedssh.firebaseio.com/items.json', function(data){
-    console.log(data);
+
+  $(document).ready(function(){
+    var url = 'https://savingsmultipliedssh.firebaseio.com/items.json';
+
+
+     $.get(url, function(data){
+       var $image = data[0].image,
+           $seller = data[0].seller,
+           $title = data[0].title,
+           $price = data[0].price,
+           $endDate = data[0].endDate;
+           $timeCalc = moment($endDate).fromNow();
+
+
+       var $item = $('<div class="sortresults-list"><img src="' + $image + '" class="green-container"><div>' + $seller + '</div><div>' + $title + '</div></div>');
+
+       $('.sort-results').append($item);
+     });
+
+
   });
-
-
+ 
 
 }());
