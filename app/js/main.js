85f319bdb7fe9b0070f7e73a4edba4e3c53897c9
mollytaryn <mollytaryn@gmail.com>
2015-02-04 20:57:22 -0600

    looped items, updated classes in sass and fixed alignment, finished moment function

--- app/js/main.js
+++ app/js/main.js
@@ -3,24 +3,25 @@
 
   $(document).ready(function(){
     var url = 'https://savingsmultipliedssh.firebaseio.com/items.json';
-
-
      $.get(url, function(data){
-       var $image = data[0].image,
-           $seller = data[0].seller,
-           $title = data[0].title,
-           $price = data[0].price,
-           $endDate = data[0].endDate;
-           $timeCalc = moment($endDate).fromNow();
 
+       _.forEach(data, function(item){
+         var $endDate = item.endDate;
+         var $timeCalc = moment($endDate).fromNow();
 
-       var $item = $('<div class="sortresults-list"><img src="' + $image + '" class="green-container"><div>' + $seller + '</div><div>' + $title + '</div></div>');
+         var $image = item.image,
+             $seller = item.seller,
+             $title = item.title,
+             $price = item.price;
 
-       $('.sort-results').append($item);
+         var $item = $('<div class="sortresults-list"><img src="'
+                       + $image + '" class="green-container"><div class="text">'
+                       + $seller + '</div><div class= "text">'
+                       + $title + '</div><div class="text">'
+                       + $timeCalc + '</div></div>');
+
+         $('.sort-results').append($item);
+       });
      });
-
-
   });
- 
-
 }());
